029981764700114fb8d2246dbf59460c5d65ff83
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2013-09-28 15:12:10 +0200

    Disabled DirectoryIterator, protected vars and public constructor, added debugging functions and hooks

--- robodt.php
+++ robodt.php
@@ -16,22 +16,39 @@ require 'settings.php';
 use Robodt\Hooks;
 use Robodt\Actions;
 use Robodt\Settings;
-use DirectoryIterator;
+
+// use DirectoryIterator;
 use dflydev\markdown\MarkdownParser as MarkdownParser;
 
 class Robodt
 {
 
-	var $hooks;
-	var $actions;
-	var $settings;
-	var $api;
+	protected $hooks;
+	protected $actions;
+	protected $settings;
+	protected $api;
 
-	function __construct() {
+	public function __construct() {
 		$this->hooks = new Hooks;
 		$this->actions = new Actions;
 		$this->settings = new Settings;
 		$this->api = array();
+
+		// DEBUG: hardcoded markdown parser, change it!
+		$markdown = new MarkdownParser();
+
+		// Register hooks and actions
+		$this->actions->register('set_site', 'set_site', $this);
+		$this->actions->register('render_markdown', 'transformMarkdown', $markdown);
+
+		// DEBUG: debug hooks, to be removed remove later on
+		$this->hooks->register('debug', 'debug_print_settings', $this, 5);
+		$this->hooks->register('debug', 'debug_print_api', $this, 1);
+
+		// DEBUG: hard coded settings, change it!
+		$this->settings->set('dir.sites', 'sites');
+		$this->settings->set('dir.content', 'content');
+		$this->settings->set('dir.themes', 'themes');
 	}
 
 	/*
@@ -45,9 +62,11 @@ class Robodt
 		- Settings?
 	*/
 
-	public function render($uri, $site) {
+	public function render($uri, $site = false) {
 		$this->hooks->execute('init');
-		$this->hooks->execute('site');
+		$this->actions->execute('set_site', array($site));
+		print $this->actions->execute('render_markdown', array($uri));
+		$this->hooks->execute('debug');
 	}
 
 	public function set_site($site = false) {
@@ -62,6 +81,20 @@ class Robodt
 		$this->api['site'] = $site;
 
 		return $site;
+	}
+
+	public function debug_print_api() {
+		print "<h3>API</h3>\n";
+		print "<pre>\n";
+		print_r($this->api);
+		print "\n</pre><hr />";
+	}
+
+	public function debug_print_settings() {
+		print "<h3>Settings</h3>\n";
+		print "<pre>\n";
+		print_r($this->settings->get_all());
+		print "\n</pre><hr />";
 	}
 
 }
