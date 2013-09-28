499be1b7d2313dd8b5f4f1d225fa6f0e03462d9b
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2013-09-28 21:40:23 +0200

    Test implementation file tree generator

--- robodt.php
+++ robodt.php
@@ -17,7 +17,7 @@ use Robodt\Hooks;
 use Robodt\Actions;
 use Robodt\Settings;
 
-// use DirectoryIterator;
+use DirectoryIterator;
 use dflydev\markdown\MarkdownParser as MarkdownParser;
 
 class Robodt
@@ -28,6 +28,7 @@ class Robodt
 	protected $settings;
 	protected $api;
 
+
 	public function __construct() {
 		$this->hooks = new Hooks;
 		$this->actions = new Actions;
@@ -35,11 +36,11 @@ class Robodt
 		$this->api = array();
 
 		// DEBUG: hardcoded markdown parser, change it!
-		$markdown = new MarkdownParser();
+		// $markdown = new MarkdownParser();
 
 		// Register hooks and actions
 		$this->actions->register('site.set', 'setSite', $this);
-		$this->actions->register('request.render', 'transformMarkdown', $markdown);
+		$this->actions->register('request.render', 'requestRender', $this);
 
 		// DEBUG: debug hooks, to be removed remove later on
 		$this->hooks->register('debug', 'debugSettings', $this, 10);
@@ -51,13 +52,42 @@ class Robodt
 		$this->settings->set('dir.themes', 'themes');
 	}
 
+
 	// TODO: implement
 	public function render($uri, $site = false) {
 		$this->hooks->execute('init');
 		$this->actions->execute('site.set', array($site));
-		print $this->actions->execute('request.render', array($uri));
+		$this->actions->execute('request.render', array($uri));
 		$this->hooks->execute('debug');
 	}
+
+
+	public function requestRender($uri) {
+		print "<h3>Content Tree</h3>\n<pre>\n";
+		$content = array('.', 'sites', 'default', 'contents');
+		$content = implode(DIRECTORY_SEPARATOR, $content);
+		$content = new DirectoryIterator($content);
+		print_r($this->generateTree($content));
+		print "\n</pre><hr />";
+	}
+
+
+	public function generateTree( DirectoryIterator $dir )
+	{
+		$data = array();
+		foreach ( $dir as $node ) {
+			if ( $node->isDir() && !$node->isDot() ) {
+				$data[$node->getFilename()] = $this->generateTree( new DirectoryIterator( $node->getPathname() ) );
+			}
+			else if ( $node->isFile() ) {
+				$data[] = $node->getFilename();
+			}
+		}
+		// $data = preg_grep('/^([^.])/', $data);
+
+		return $data;
+	}
+
 
 	public function setSite($site = false) {
 		if ( ! $site) {
@@ -73,10 +103,12 @@ class Robodt
 		return $site;
 	}
 
+
 	private function splitFile($contents) {
 		$contents = str_replace("\r", "", $contents);
 		return preg_split('![\r\n]+[-]{4,}!i', $contents);
 	}
+
 
 	private function parseMetadata($contents) {
 		// Generate array from string
@@ -90,6 +122,7 @@ class Robodt
 		return array_map('trim', $metadata);
 	}
 
+
 	public function debugApi() {
 		print "<h3>API</h3>\n";
 		print "<pre>\n";
@@ -97,11 +130,13 @@ class Robodt
 		print "\n</pre><hr />";
 	}
 
+
 	public function debugSettings() {
 		print "<h3>Settings</h3>\n";
 		print "<pre>\n";
 		print_r($this->settings->get_all());
 		print "\n</pre><hr />";
 	}
+
 
 }
