fa56c33b91455b3cfbde4750b46ad962ea14eefd
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2013-10-20 02:42:53 +0200

    Implemented content and meta parsing

--- content.php
+++ content.php
@@ -17,7 +17,73 @@ class Content
 	// Content types
 	// Fields
 
-	// DEBUG: hardcoded markdown parser, change it!
-	// $markdown = new MarkdownParser();
+
+	private $markdown;
+
+
+	public function __construct() {
+		$this->markdown = new MarkdownParser;
+	}
+
+
+	/**
+	 * Parse markdown file
+	 */
+	public function parseFile($file) {
+		// File found
+		if (file_exists($file)) {
+			$file = file_get_contents($file, FILE_USE_INCLUDE_PATH);
+			$file = $this->splitFile($file);
+
+			// Metadata found
+			if (count($file) > 1) {
+				return array(
+					'content' => $this->parseMarkdown($file[1]),
+					'metadata' => $this->parseMetadata($file[0])
+					);
+			}
+
+			// No metadata found
+			else {
+				return array('content' => $this->parseMarkdown($file[0]));
+			}
+		}
+
+		// File not found, return false
+		else {
+			return false;
+		}
+	}
+
+
+	/**
+	 * Split metadata from content
+	 */
+	private function splitFile($contents)
+	{
+		$contents = str_replace("\r", "", $contents);
+		return preg_split('![\r\n]+[-]{4,}!i', $contents);
+	}
+
+
+	/**
+	 * Parse metadata
+	 */
+	private function parseMetadata($contents)
+	{
+		$metadata = array();
+		preg_match_all('/^(.+?):(.+)$/m', $contents, $metadata);
+		$metadata = array_combine($metadata[1], $metadata[2]);
+		return array_map('trim', $metadata);
+	}
+
+
+	/**
+	 * Parse markdown content
+	 */
+	public function parseMarkdown($contents)
+	{
+		return $this->markdown->transformMarkdown($contents);
+	}
 
 }
