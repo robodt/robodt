ad7f60a31f4c9e51d834bd1dbbe41ef2ed624a09
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2014-01-30 08:45:55 +0100

    Robodt interfaces for implementation of frameworks

---
+++ src/Robodt/Interfaces.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Robodt - Markdown CMS
+ * @author      Zomnium
+ * @link        http://www.zomnium.com
+ * @copyright   2014 Zomnium, Tim van Bergenhenegouwen
+ */
+
+namespace Robodt;
+
+interface RobodtRouter
+{
+    /* routing */
+
+    public function get($route, $callback);
+
+    public function post($route, $callback);
+
+    public function put($route, $callback);
+
+    public function delete($route, $callback);
+}
+
+interface RobodtLogger
+{
+    /* logging */
+
+    public function add($record);
+}
+
+interface RobodtHooks
+{
+    /* hooks */
+
+    public function set($name, $callback);
+
+    public function remove($name);
+
+    public function apply($name);
+}
+
+interface RobodtErrorhandling
+{
+    /* error handling */
+
+    public function error();
+}
+
+interface RobodtViews
+{
+    /* theming */
+
+    public function get();
+
+    public function set();
+}
+
+    // Interface?
+    // Framework implementation?
+
+    // Routing / Router - interface
+    // Logging / Logger - interface - with fallback?
+    // Hooks - interface - with fallback
+
+    // Theming / Template (handler)
+    // Boostrapping / Bootstrap
+
+    // Extendable (routes, admin, plugins, etc.)
+
+
+    // Functions:
+    // - __construct: site
+    // - mainRoute: route
+    // - get: route, callback
+    // - run
+
+interface RobodtExtended
+{
+    /* dependency injection */
+
+    public function setNamespace($namespace, $resource);
+
+    public function getNamespace();
+
+    /* request */
+
+    public function headersRequest();
+
+    public function cookiesRequest();
+
+    /* caching */
+
+    public function httpCaching();
+
+    /* sesssion */
+
+    public function getSession();
+
+    public function setSession();
+
+    /* flash messages */
+
+    public function flashMessage(); // now, next, keep
+
+}
